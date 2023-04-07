133f046fd91ae3e0b36695edc432e389558381c7
Mustafa <65766186+gtcalifornia@users.noreply.github.com>
2023-04-07 18:28:16 +0200

    Update freebie.js

--- freebie.js
+++ freebie.js
@@ -1,25 +1,137 @@
-
-const cheapTickets = []
+const cheapTickets = [];
+const GOOD_DEAL_PRICE_THRESHOLD = 25;
+const MODERATE_PRICE_THRESHOLD = 50;
+const HIGH_PRICE_THRESHOLD = 100;
+const VERY_HIGH_PRICE_THRESHOLD = 150;
 
 /**
  * Converts a string to a floating-point number.
  * 
- * @param {string} a - The string to convert.
+ * @param {string} str - The string to convert.
  * @returns {number} The converted floating-point number.
  */
-function convertToFloat(a) {
-    return parseFloat(a);
+function convertToFloat(str) {
+  return parseFloat(str);
+}
+
+
+/**
+Checks if the user is online.
+If the user is offline, it displays an error message.
+*/
+function checkOnlineStatus() {
+  if (!navigator.onLine) {
+    const errorMessage = document.getElementById("error_message");
+    errorMessage.innerHTML = "You are not connected to the internet!";
+    errorMessage.style.fontSize = '16px';
+    errorMessage.style.color = 'red';
+    return;
   }
+}
+
+/**
+
+Displays a loading spinner while waiting for data to load.
+*/
+function showLoadingSpinner() {
+      // Display a loading spinner or bar
+      const loadingSpinner = document.getElementById("loading-spinner");
+      loadingSpinner.style.display = "block";
+}
+
+/**
+Shows sort buttons and search input.
+*/
+function showSortButtons() {
+    // Make the sort buttons and search input visible
+    document.getElementById("sort-button").style.visibility = "visible"
+    document.getElementById("sort-button-by-price").style.visibility = "visible"
+    document.getElementById("sort-button-by-date").style.visibility = "visible";
+    document.getElementById("input-group").classList.toggle("d-none");
+}
+
+/**
+Sorts the search results by country name in ascending or descending order.
+@param {string} sortOrder - The sort order. It can be "asc" for ascending order or "desc" for descending order.
+@returns {string} - The updated sort order.
+*/
+function sortResultsByCountryName(sortOrder) {
+  const searchResultsBox = document.getElementById('search_results_box');
+  const resultItems = searchResultsBox.querySelectorAll('.result-item');
+
+  // Convert the NodeList to an array so we can sort it
+  const itemsArray = Array.from(resultItems);
+
+  // Sort the items by departure place
+  itemsArray.sort(function(a, b) {
+    const aDeparture = a.querySelector('.result-title').textContent;
+    const bDeparture = b.querySelector('.result-title').textContent;
+    if (sortOrder === 'asc') {
+      return aDeparture.localeCompare(bDeparture);
+    } else {
+      return bDeparture.localeCompare(aDeparture);
+    }
+  });
+
+  // Empty the search results box
+  searchResultsBox.innerHTML = "";
+
+  // Add the sorted items back to the search results box
+  itemsArray.forEach(function(item) {
+    searchResultsBox.appendChild(item);
+  });
+
+  // toggle the sort order
+  if (sortOrder === 'asc') {
+    sortOrder = 'desc';
+  } else {
+    sortOrder = 'asc';
+  }
+
+  return sortOrder;
+}
+
+/**
+Sorts the search results by price in ascending or descending order.
+*/
+let sortOrderByPrice = 'asc';
+function sortByPrice() {
+  const searchResultsBox = document.getElementById('search_results_box');
+  const resultItems = searchResultsBox.querySelectorAll('.result-item');
+  
+  // Convert the NodeList to an array so we can sort it
+  const itemsArray = Array.from(resultItems);
+  
+  // Sort the items by price
+  itemsArray.sort(function(a, b) {
+    const aPrice = parseFloat(a.querySelector('.result-description').textContent.split(' - ')[1]);
+    const bPrice = parseFloat(b.querySelector('.result-description').textContent.split(' - ')[1]);
+    
+    if (sortOrderByPrice === 'asc') {
+      return aPrice - bPrice;
+    } else {
+      return bPrice - aPrice;
+    }
+  });
+  
+  // Empty the search results box
+  searchResultsBox.innerHTML = "";
+  
+  // Add the sorted items back to the search results box
+  itemsArray.forEach(function(item) {
+    searchResultsBox.appendChild(item);
+  });
+  
+  // Toggle the sort order
+  sortOrderByPrice = sortOrderByPrice === 'asc' ? 'desc' : 'asc';
+}
+
+
 
 // This is an async function that sends a GET request to the Ryanair API with the specified parameters and displays the search results on the page
 async function doGetRequest() {
-    if (!navigator.onLine) {
-        const errorMessage = document.getElementById("error_message");
-        errorMessage.innerHTML = "You are not connected to the internet!";
-        errorMessage.style.fontSize = '16px';
-        errorMessage.style.color = 'red';
-        return;
-    }
+    checkOnlineStatus()
+
     // Display a loading spinner or bar
     const loadingSpinner = document.getElementById("loading-spinner");
     loadingSpinner.style.display = "block";
@@ -43,10 +155,7 @@ async function doGetRequest() {
         loadingSpinner.style.display = "none";
       }, 500);
 
-    // Make the sort buttons and search input visible
-    document.getElementById("sort-button").style.visibility = "visible"
-    document.getElementById("sort-button-by-price").style.visibility = "visible"
-    document.getElementById("input-group").classList.toggle("d-none");
+    showSortButtons()
     
     // Change the search button color and label
     var searchButton = document.getElementById("search-button")
@@ -58,86 +167,27 @@ async function doGetRequest() {
     var searchResultsBox = document.getElementById('search_results_box');
     searchResultsBox.innerHTML = ""; // clear existing search results
     
-    // Sort the search results by price (ascending by default)
-    fares.sort((a, b) => convertToFloat(a.outbound.price.value) - convertToFloat(b.outbound.price.value));
+    
     const sortButton = document.getElementById("sort-button");
     const sortButtonByPrice = document.getElementById("sort-button-by-price");
-    let sortOrderPrice = "asc";
+
+
+    // Sort the search results by price (ascending by default)
+    fares.sort((a, b) => convertToFloat(a.outbound.price.value) - convertToFloat(b.outbound.price.value));
+
     
-    // Sort by Country Name
+    //-----------------------------------------SORT BY COUNTRY NAME--------------------------
     let sortOrder = 'asc';
-
-    // Add an event listener to the sort by country name button
     sortButton.addEventListener("click", function() {
-    const searchResultsBox = document.getElementById('search_results_box');
-    
-    const resultItems = searchResultsBox.querySelectorAll('.result-item');
-
-    // Convert the NodeList to an array so we can sort it
-    const itemsArray = Array.from(resultItems);
-
-    // sort the items by departure place
-    itemsArray.sort(function(a, b) {
-        const aDeparture = a.querySelector('.result-title').textContent;
-        const bDeparture = b.querySelector('.result-title').textContent;
-        if (sortOrder === 'asc') {
-        return aDeparture.localeCompare(bDeparture);
-        } else {
-        return bDeparture.localeCompare(aDeparture);
-        }
+    sortOrder = sortResultsByCountryName(sortOrder);
     });
 
-    // empty the search results box
-    searchResultsBox.innerHTML = "";
+    //-----------------------------------------SORT BY PRICE--------------------------
 
-    // add the sorted items back to the search results box
-    itemsArray.forEach(function(item) {
-        searchResultsBox.appendChild(item);
-    });
-
-    // toggle the sort order
-    sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
-    });
-    // Sort by country name is Done.
-    //-----------------------
     sortButtonByPrice.addEventListener('click', function() {
-        const searchResultsBox = document.getElementById('search_results_box');
-        
-        const resultItems = searchResultsBox.querySelectorAll('.result-item');
-        
-        // convert the NodeList to an Array
-        const itemsArray = Array.from(resultItems);
-        
-        const sortDirection = this.dataset.sortDirection;
-        
-        // sort the items by price
-        itemsArray.sort(function(a, b) {
-          const aPrice = convertToFloat(a.querySelector('.result-description').textContent.split(' - ')[1]);
-          const bPrice = convertToFloat(b.querySelector('.result-description').textContent.split(' - ')[1]);
-          
-          if (sortDirection === 'asc') {
-            return aPrice - bPrice;
-          } else {
-            return bPrice - aPrice;
-          }
-        });
-        
-        // empty the search results box
-        searchResultsBox.innerHTML = "";
-        
-        // add the sorted items back to the search results box
-        itemsArray.forEach(function(item) {
-          searchResultsBox.appendChild(item);
-        });
-        
-        // toggle the sort direction
-        if (sortDirection === 'asc') {
-          sortButtonByPrice.dataset.sortDirection = 'desc';
-        } else {
-          sortButtonByPrice.dataset.sortDirection = 'asc';
-        }
+      sortByPrice()
       });
-      
+
       if (data.total > 1) { // Check if there are search results
         welcomeMessage.style.fontSize = '12px'; // Set font size for welcome message
         welcomeMessage.innerHTML = `Departure Airport: ${fromWhere} </br> The places you can go between ${startDate} - ${endDate}`; // Set text for welcome message
@@ -145,7 +195,7 @@ async function doGetRequest() {
         console.log(welcomeMessage); // Log welcome message to console
     
         for (let i = 0; i < 30; i++) { // Loop through the first 30 search results
-            var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; // Array of days of the week
+            // var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; // Array of days of the week
     
             // Get flight information for current search result
             const destination = `${data.fares[i].outbound.arrivalAirport.countryName}, ${data.fares[i].outbound.arrivalAirport.name}`;
@@ -188,19 +238,20 @@ async function doGetRequest() {
             searchResultsBox.appendChild(searchResultBox);
     
             // Check price of flight and assign background color to search result based on price range
-            let priceValue = convertToFloat(data.fares[i].outbound.price.value);
-            if (priceValue <= 25) {
+            const priceValue = convertToFloat(data.fares[i].outbound.price.value);
+
+            if (priceValue <= GOOD_DEAL_PRICE_THRESHOLD) {
                 const badgeIcon = document.createElement('span');
                 badgeIcon.className = 'badge-icon';
                 badgeIcon.textContent = '🏆 Good Deal';
                 resultItem.style.backgroundColor = "#03C988";
                 resultItem.appendChild(badgeIcon);
             } 
-            else if (priceValue < 50) {
+            else if (priceValue < MODERATE_PRICE_THRESHOLD) {
                 resultItem.style.backgroundColor = "#03C988";
-            } else if (priceValue >= 50 && priceValue < 100) {
+            } else if (priceValue >= MODERATE_PRICE_THRESHOLD && priceValue < 100) {
                 resultItem.style.backgroundColor = '#2F58CD';
-            } else if (priceValue >= 100 && priceValue < 150){
+            } else if (priceValue >= HIGH_PRICE_THRESHOLD && priceValue < VERY_HIGH_PRICE_THRESHOLD){
                 resultItem.style.backgroundColor = '#3A1078';
             }
             else {
